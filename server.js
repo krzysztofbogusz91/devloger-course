5dd7dac2a73917b3d5779839c0e81643b026929b
kfbz <krzysztof.bogusz@gft.com>
2018-10-18 17:57:44 +0200

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const db = require('./config/keys').db;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('mongo db connected');
+  })
+  .catch(err => {
+    console.log('err', err);
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on ${port}`));
